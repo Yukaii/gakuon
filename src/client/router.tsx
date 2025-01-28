fd9d28f408c94a3428d23e56cedeb6c09b3a78ca
Yukai Huang <yukaihuangtw@gmail.com>
2025-01-28 18:16:17 +0800

    chore: drop cardview

--- src/client/router.tsx
+++ src/client/router.tsx
@@ -1,6 +1,5 @@
 import { Navigate, createHashRouter } from "react-router-dom";
 import { DeckView } from "./views/DeckView";
-import { CardView } from "./views/CardView";
 import { RootLayout } from "./layouts/RootLayout";
 
 export const router = createHashRouter([
@@ -19,10 +18,6 @@ export const router = createHashRouter([
       {
         path: "decks/:deckName",
         element: <DeckView />,
-      },
-      {
-        path: "decks/:deckName/cards/:cardId",
-        element: <CardView />,
       },
     ],
   },
